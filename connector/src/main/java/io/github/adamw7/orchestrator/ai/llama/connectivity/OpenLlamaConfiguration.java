87eba5785b218e3ab339c916528ac40459ff9cc7
adamw7 <adamwitkowski3@gmail.com>
2024-05-29 13:10:05 +0200

    Updating Open Llama connectivity

--- connector/src/main/java/io/github/adamw7/orchestrator/ai/llama/connectivity/OpenLlamaConfiguration.java
+++ connector/src/main/java/io/github/adamw7/orchestrator/ai/llama/connectivity/OpenLlamaConfiguration.java
@@ -1,9 +1,9 @@
 package io.github.adamw7.orchestrator.ai.llama.connectivity;
 
 final class OpenLlamaConfiguration {
-    public static final String API_KEY = "aie93JaTv1GW1AP4IIUSqeecV22HgpcQ6WlgWNyfx2HflkY5hTw19JDbT90ViKcZaZ6lpjOo3YIGgpkG7Zb8jEKvdM5Ymnq9jPm79osLppCebwJ7WdWTwWq3Rf15NDxm";
-    private static final int PORT = 8000;
-    private static final String HOST = "http://20.86.28.184";
+    public static final String API_KEY = "WDdOH68a2LRsFUJWgWaCBtVLoOR1jR6miKRA1lYC2Pm7m2KVqgChDkwKNR9eVTBf";
+    private static final int PORT = 8080;
+    private static final String HOST = "http://23.97.191.61";
     private static final String ENDPOINT = "/completion";
     static final String URL = HOST + ":" + PORT + ENDPOINT;
     private static final String ENDPOINT_TOKENIZE = "/tokenize";
